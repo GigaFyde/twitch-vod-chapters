3e529647536107ac0df946d8a19fb7e52f37f37d
Laurant Marijnissen <laurant@gigafyde.dev>
2023-05-15 13:22:36 +0200

    Use a default client-id

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ module.exports = async (event, context) => {
         method: 'POST',
         body: `[{"operationName":"VideoPlayer_ChapterSelectButtonVideo","variables\":{"includePrivate":false,"videoID":"${event.body.video_id}" },"extensions":{"persistedQuery":{"version":1,"sha256Hash":"8d2793384aac3773beab5e59bd5d6f585aedb923d292800119e03d40cd0f9b41"}}}]`
         , headers: {
-            "authorization": `OAuth ${event.body.oauth}`
+            "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko"
         }});
     const data = await response.json();
     let result = [];
